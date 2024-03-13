051c273f3a817d2cb0f792e9ede1ab1d3f9249d3
Johyuncheol <finos3703@naver.com>
2024-03-13 17:02:42 +0900

    [feat-detail]: 서브모듈 추가
    shared 공유모듈브랜치를 서브모듈로 추가

--- services/detail/src/App.tsx
+++ services/detail/src/App.tsx
@@ -1,16 +1,16 @@
 import React, { Suspense, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import Globalstyles from "../../../shared/globalStyle/GlobalStyle";
+import Globalstyles from "../../../shared/shared/globalStyle/GlobalStyle";
 import DetailPage from "./pages/DetailPage";
 import { BrowserRouter } from "react-router-dom";
-import Layout from "../../../shared/layout/Layout";
+import Layout from "../../../shared/shared/layout/Layout";
 function App() {
   return (
     <BrowserRouter>
       <Suspense fallback={<div>Loading...</div>}>
         <Globalstyles />
         <Routes>
-          <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Layout headerType={"sub"} />}>
             <Route path="/detail/:id" element={<DetailPage />} />
           </Route>
         </Routes>
