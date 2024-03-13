746c845698799809cf85851febb2d4c8652df4a5
Johyuncheol <finos3703@naver.com>
2024-03-14 03:42:44 +0900

    [fix-shared]: 커스텀링크 수정
    커스텀링크 같은 서비스인지 확인하는 부분에서 로직이 수정

--- shared/components/header/CustomLink.tsx
+++ shared/components/header/CustomLink.tsx
@@ -5,28 +5,25 @@ import { useLocation } from "react-router-dom";
 interface ConditionalLinkProps {
   to: string;
   children: React.ReactNode;
-  mouseOverFunc?: () => void;
-  onClickFunc?: () => void;
+  mouseOverFunc?:() => void;
+  onClickFunc?:() => void;
 }
 
 const ConditionalLink: React.FC<ConditionalLinkProps> = ({
   to,
   children,
-  mouseOverFunc,
+  mouseOverFunc
 }) => {
-  const { pathname } = useLocation();
-  const serviceInitial = pathname.split("/")[1];
-  const toServiceName = to.split("/").shift();
-  const isLink = toServiceName ? toServiceName.includes(serviceInitial) : false;
-
+  
+  const {pathname}=useLocation();
+  const nowService= pathname.split('/')[1];
+  const toService= to.split('/')[1]
+  const isLink = nowService===toService;
+  
   return isLink ? (
-    <Link to={to} onMouseOver={mouseOverFunc}>
-      {children}
-    </Link>
+    <Link to={to} onMouseOver={mouseOverFunc}>{children}</Link>
   ) : (
-    <a href={to} onMouseOver={mouseOverFunc}>
-      {children}
-    </a>
+    <a href={to} onMouseOver={mouseOverFunc}>{children}</a>
   );
 };
 
